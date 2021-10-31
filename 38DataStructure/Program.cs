3c4be6927eaf397c002cf5ca4f884f081e1484c3
KoSunHyo <83582728+ttanker@users.noreply.github.com>
2021-10-31 21:38:15 +0900

    Add files via upload

---
+++ 38DataStructure/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//자료구조
+//자료구조를 직접 만들어 보는건
+//게임 프로그래머의 언어와 같이 기초소양이다.
+//총알이 1000발이 발사된다. 어떻게 관리하지?
+//NPC가 500명이 있다. 그중에서 '피오나'
+//라는 이름의 NPC가 있는데 어떻게 찾지?
+
+class MyDataStruct<T>
+{
+    //넣는다.()
+    //탐색. ()
+    //확장한다. ()
+    public void Push(T _Data)
+    {
+        //if(/*이 자료가 들어왔을 때 내 사이즈를 오버하면 */)
+        //{
+        //    MDS.Ex(/*적절한 수*/);
+        //}
+
+        //여러가지 예외처리해야 할 것이 있다.
+    }
+    public int Find(T _Data)
+    {
+        return 100;
+    }
+    public void Ex(int _Size)
+    {
+
+    }
+}
+
+namespace _38DataStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //자료구조라는 건
+            //int 10
+            //0 1 2 3 4 5 6 7 8 9
+
+            //자료구조에는
+            //다음의 구조나 인터페이스를 지원한다.
+            //함수로 표현한다.
+            //넣는다.()
+            //탐색. ()
+            //확장한다. ()
+
+
+            //자료구조에는 보통 컨테이너라는 
+            //용어를 사용하는데
+            //시퀀스 컨테이너(넣어주면 넣어주는대로
+            //차곡차고 쌓임)와 List(c#) vecter (c#에서는 없다. c/c++)
+            //연관 컨테이너(예, 어떠한 규칙에 따라 값 말고 순서가 바뀜) Dictionary(c# 해쉬 맵) map(c++)
+            //어뎁터 컨테이너라고 분류. stack queue
+
+            //데이터의 메모리 구조는
+            //배열과
+            //노드형
+
+            //자료구조이다.
+            int[] Arr = new int[10];
+            Arr[0] = 0;
+            Arr[1] = 1;
+
+            MyDataStruct<int> MDS = new MyDataStruct<int>();
+
+            //100을 넣어줘
+            MDS.Push(100);
+
+            //50을 찾아줘
+            MDS.Find(50);
+
+            //자동으로 이루어진다.
+            //MDS.Ex(50000);
+
+        }
+    }
+}
---
+++ List/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class item
+{
+
+}
+
+class MyList<T>
+{
+    int[] Arr = new int [0];
+    int Capa;
+    int Count;
+
+    public void Add(T _Add)
+    {
+        if (Count + 1 >= Capa)
+        {
+            //확장
+        }
+    }
+}
+
+namespace List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> NewInt = new MyList<int>();
+
+            NewInt.Add(100);
+
+            //넣는 함수
+            //찾는 함수
+            //지우는 함수
+            
+            //배열형
+            //시퀸스
+
+            //내가 관리해달라고 요청하는 것이
+            //매번 바뀌는 것.
+            //그러므로 제네릭 클래스이여야 한다.
+            List<int> NewList = new List<int>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine((NewList.Count + 1).ToString() + "Add");
+                Console.WriteLine("Capacity" + NewList.Capacity);//배열의 크기
+                Console.WriteLine("Count" + NewList.Count); //자료의 크기
+                NewList.Add(i);
+            }
+            //자료 : 넣어준 int
+
+            Console.WriteLine("");
+            Console.WriteLine("Contains");
+
+            //탐색에는 유리하지 않은 자료구조라고 본다.
+            //인덱스로 표현할수 있다면 탐색에 가장 유리한 자료구조다.
+            //내부에 자료가 있는지 없는지 있니? 없니?
+            if (NewList.Contains(8))
+            {
+                Console.WriteLine("내부에 8이 있습니다.");
+            }
+
+            if (NewList.Contains(99999999))
+            {
+                Console.WriteLine("내부에 99999999이 있습니다.");
+            } else 
+            {
+                Console.WriteLine("내부에 99999999이 없습니다.");
+            }
+
+            //인덱서 혹은 연산자 겹지정이라고 부르는데.
+            Console.WriteLine(NewList[5]);
+
+            NewList.Remove(5); // 10이 있다면 지운다.
+            NewList.RemoveAt(1); // 첫번째를 지운다.
+                                 //NewList.RemoveAll(); //다른 리스트 혹은 배열을 넣어줬을 때
+                                 //비교하여 중복되면 지운다. 
+
+            NewList.RemoveRange(0, 2); //0번째부터 4개 지워라.
+
+            for(int i = 0; i < NewList.Count; i++)
+            {
+                Console.WriteLine(NewList[1]);
+            }
+            Console.WriteLine("");
+            //리스트은 중간에 있는 애를 쏙 뺄 수 있음
+            //배열은 불가
+            int[] Arr = NewList.ToArray();
+            //(Argumaent)OutOfRange(Exception)
+            //자료의 개수로 판단하지
+            //배열의 크기는 중요하지 않다. 
+            // Console.WriteLine(NewList[999]);
+
+        }
+    }
+}
